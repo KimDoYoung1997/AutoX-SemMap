9ab4e031334f59f1ab45a950ba644cf791106459
KimDoYoung1997 <ehdud4520@naver.com>
2025-02-06 11:06:25 +0900

    replace ros_numpy's numpify with opencv and first commit

--- fetch_ws/devel/share/fetchit_challenge/cmake/fetchit_challenge-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(fetchit_challenge_MSG_INCLUDE_DIRS "/root/noetic_cuda_pt/AutoX-SemMap/fetch_ws/devel/share/fetchit_challenge/msg")
-set(fetchit_challenge_MSG_DEPENDENCIES actionlib_msgs)
